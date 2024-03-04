7464b04f79db38bf765254abba6d7a8cd469424c
Abdoul Aziz BALDE <59298812+baldazi@users.noreply.github.com>
2024-03-04 23:57:13 +0100

    img for stage

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import ProjectCard from '../components/projectCard';
 import conflistikImg from '../assets/img/conflistik.png';
+import stageImg from '../assets/img/stage.png';
 function Projet() {
     return (
         <div className="min-h-screen">
             <div className="max-w-lg md:max-w-[92%] mx-auto">
                 {/* mobile application project */}
                 <div className="divider divider-primary divider-start"><span className="badge badge-lg badge-primary">Mobile App</span></div>
-                    <div className="flex gap-5">
-                        {/* conflistik card */}
-                        <ProjectCard title="Conflistik" description="" image={conflistikImg}/>
-                        {/* stage card */}
-                        <ProjectCard title="Stage" description="" image={conflistikImg}/>
-                    </div>     
+                <div className="flex gap-5">
+                    {/* conflistik card */}
+                    <ProjectCard title="Conflistik" description="" image={conflistikImg} />
+                    {/* stage card */}
+                    <ProjectCard title="Stage" description="" image={stageImg} />
+                </div>
 
                 {/* Website */}
-                <div className="divider divider-primary divider-start"><span className="badge badge-lg badge-primary">Website Application</span></div> 
-                {/* stage card */}
-                <ProjectCard title="Stage" description="" image={conflistikImg}/>      
+                <div className="divider divider-secondary divider-start"><span className="badge badge-lg badge-secondary">Website Application</span></div>
+                <div className="flex gap-5">
+                    {/* stage card */}
+                    <ProjectCard title="Stage" description="" image={conflistikImg} />
+                </div>
+
+                {/* other */}
+                <div className="divider divider-accent divider-start"><span className="badge badge-lg badge-accent">Others</span></div>
+                <div className="flex gap-5">
+                    {/* stage card */}
+                    <ProjectCard title="Stage" description="" image={conflistikImg} />
+                </div>
             </div>
 
 
