3c8ba5d924638b4055d064c8a1d49104ece7fa00
Abdoul Aziz BALDE <59298812+baldazi@users.noreply.github.com>
2024-03-04 23:29:05 +0100

    Update project.jsx
    website show project template

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -5,15 +5,22 @@ function Projet() {
     return (
         <div className="min-h-screen">
             <div className="max-w-lg md:max-w-[92%] mx-auto">
-            {/* mobile application project */}
-            <div className="divider divider-primary divider-start"><span className="badge badge-lg badge-primary">Mobile App</span></div>
-                <div className="flex gap-5">
-                    {/* conflistik card */}
-                    <ProjectCard title="Conflistik" description="" image={conflistikImg}/>
-                    {/* stage card */}
-                    <ProjectCard title="Stage" description="" image={conflistikImg}/>
-                </div>            
+                {/* mobile application project */}
+                <div className="divider divider-primary divider-start"><span className="badge badge-lg badge-primary">Mobile App</span></div>
+                    <div className="flex gap-5">
+                        {/* conflistik card */}
+                        <ProjectCard title="Conflistik" description="" image={conflistikImg}/>
+                        {/* stage card */}
+                        <ProjectCard title="Stage" description="" image={conflistikImg}/>
+                    </div>     
+
+                {/* Website */}
+                <div className="divider divider-primary divider-start"><span className="badge badge-lg badge-primary">Website Application</span></div> 
+                {/* stage card */}
+                <ProjectCard title="Stage" description="" image={conflistikImg}/>      
             </div>
+
+
         </div>
     );
 }
