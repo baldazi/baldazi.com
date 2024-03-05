07449b779f8255bf14cb759f4a06f274bf61083d
Abdoul Aziz BALDE <59298812+baldazi@users.noreply.github.com>
2024-03-05 12:06:52 +0100

    projet
    stage image

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -5,12 +5,12 @@ import stageImg from '../assets/img/stage.png';
 function Projet() {
     return (
         <div className="min-h-screen">
-            <div className="max-w-lg md:max-w-[92%] mx-auto">
+            <div className="max-w-lg md:max-w-[82%] mx-auto">
                 {/* mobile application project */}
                 <div className="divider divider-primary divider-start"><span className="badge badge-lg badge-primary">Mobile App</span></div>
-                <div className="flex gap-5">
+                <div className="flex gap- justify-center">
                     {/* conflistik card */}
-                    <ProjectCard title="Conflistik" description="" image={conflistikImg} />
+                    <ProjectCard title="Conflistik" description="a mobile application" image={conflistikImg} />
                     {/* stage card */}
                     <ProjectCard title="Stage" description="" image={stageImg} />
                 </div>
@@ -19,7 +19,7 @@ function Projet() {
                 <div className="divider divider-secondary divider-start"><span className="badge badge-lg badge-secondary">Website Application</span></div>
                 <div className="flex gap-5">
                     {/* stage card */}
-                    <ProjectCard title="Stage" description="" image={conflistikImg} />
+                    <ProjectCard title="Stage" description="" image={stageImg} />
                 </div>
 
                 {/* other */}
