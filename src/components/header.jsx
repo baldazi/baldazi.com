4f098aa7fa7285e668d754c55e11db17f19df205
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-10-30 23:24:57 +0100

    fix hover link behavior on the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,6 @@ import { IoClose, IoHome, IoMenu } from "react-icons/io5"
 import { BsInfoCircleFill } from "react-icons/bs"
 import { AiFillMail } from "react-icons/ai"
 import { MdWork } from "react-icons/md"
-import { TfiClose } from "react-icons/tfi"
 import LangSelector from "./langSelector";
 import { m } from "../paraglide/messages";
 import ThemePicker from "./themePicker";
@@ -14,11 +13,11 @@ function Header() {
     const navClass = "tab"
     const activeNavClass = "tab tab-active border-b-1 border-gray-700 dark:border-gray-500"
     return (
-        <header className="dark:text-gray-300 dark:hover:text-white text-gray-800 hover:text-black">
+        <header className="dark:text-gray-300 text-gray-800">
             <div className="flex justify-between items-center pt-2 pl-5 lg:px-32 bg-emerald-700 dark:bg-emerald-900 text-lg">
 
                 <div className="">
-                    <Link to="/" className="" onClick={e => setMenuToggle(false)}>
+                    <Link to="/" className="dark:hover:text-white hover:text-black" onClick={e => setMenuToggle(false)}>
                         <IoHome size={32} className="md:hidden" />
                         <span className="font-bold hidden md:inline">Baldazi</span>
                     </Link>
@@ -66,14 +65,22 @@ function Header() {
                 {/* <nav className={!menuToggle ? hideMenu : undefined}> */}
                 <nav className="hidden lg:block">
                     <ul className="tabs tabs-bordered" role="tablist">
-                        <li><NavLink role="tab" to="/" className={({ isActive }) => (isActive ? activeNavClass : navClass) + " hidden md:inline-flex"}><IoHome className="mr-1" />{m["navigation.home"]()}</NavLink></li>
-                        <li><NavLink role="tab" to="/project" className={({ isActive }) => isActive ? activeNavClass : navClass}><MdWork className="mr-1" />{m["navigation.project"]()}</NavLink></li>
-                        <li><NavLink role="tab" to="/about" className={({ isActive }) => isActive ? activeNavClass : navClass}><BsInfoCircleFill className="mr-1" />{m["navigation.about"]()}</NavLink></li>
-                        <li><NavLink role="tab" to="/contact" className={({ isActive }) => isActive ? activeNavClass : navClass}><AiFillMail className="mr-1" />{m["navigation.contact"]()}</NavLink></li>
+                        <li>
+                            <NavLink role="tab" to="/" className={({ isActive }) => (isActive ? activeNavClass : navClass)}><IoHome className="mr-1" />{m["navigation.home"]()}</NavLink>
+                        </li>
+                        <li>
+                            <NavLink role="tab" to="/project" className={({ isActive }) => isActive ? activeNavClass : navClass}><MdWork className="mr-1" />{m["navigation.project"]()}</NavLink>
+                        </li>
+                        <li>
+                            <NavLink role="tab" to="/about" className={({ isActive }) => isActive ? activeNavClass : navClass}><BsInfoCircleFill className="mr-1" />{m["navigation.about"]()}</NavLink>
+                        </li>
+                        <li>
+                            <NavLink role="tab" to="/contact" className={({ isActive }) => isActive ? activeNavClass : navClass}><AiFillMail className="mr-1" />{m["navigation.contact"]()}</NavLink>
+                        </li>
                     </ul>
                 </nav>
 
-                <div className="hidden lg:flex items-center gap-3">
+                <div className="hidden lg:flex items-center gap-3 dark:hover:text-white hover:text-black">
                     <LangSelector />
                     <ThemePicker />
                 </div>
